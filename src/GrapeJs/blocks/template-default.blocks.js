b02134a615f7ea49ceaac01dc93a841d699193b1
Trần Phi Long <50954310+goku2000vn@users.noreply.github.com>
2022-03-08 23:33:17 +0700

    2022/03/08 Long-TP Commit Demo Block Skelecton

--- src/GrapeJs/blocks/template-default.blocks.js
+++ src/GrapeJs/blocks/template-default.blocks.js
@@ -3,12 +3,13 @@ export default function loadBlocks(editor, opt = {}) {
   const c = opt;
   let bm = editor.BlockManager;
   //========| |============//
+  //#region productList
   bm.add("productList", {
-   
+
     label: `
       <div>${c.label_product_list}</div> `,
     category: c.catergory_product_list,
-    attributes : {class :"fa fa-cube"},
+    attributes: { class: "fa fa-cube" },
     content: {
       attributes: { class: "container product-section" },
       name: "Section",
@@ -323,8 +324,9 @@ export default function loadBlocks(editor, opt = {}) {
       ],
     },
   });
+  //#endregion
 
-
+  //#region carousel panel
   //LONG-TP 2022-02-22 TEST TRAITS - ADD START
   const domc = editor.DomComponents;
   const defaultType = domc.getType('default');
@@ -334,244 +336,189 @@ export default function loadBlocks(editor, opt = {}) {
   const textModel = textType.model;
   const textView = textType.view;
   const sfx = opt.socialClssfx;
-  editor.TraitManager.addType('href-next', {
-      
+  //THIS IS SETTING TRAIT
+  editor.TraitManager.addType('carousel', {
+    // Disbale label custom - set false for use createLabel below
     noLabel: true,
+    // Label custom for trait
     createLabel({ label }) {
-        return `<div>
+      return `<div>
           <div>Before</div>
           ${label}
           <div>After</div>
         </div>`;
-      },
+    },
     // Expects as return a simple HTML string or an HTML element
     createInput({ trait }) {
-      // Here we can decide to use properties from the trait
-      const traitOpts = trait.get('options') || [];
-      const options = traitOpts.length ? traitOpts : [
-        { id: 'url', name: 'URL' },
-        { id: 'email', name: 'Email' },
-      ];
-  
-      // Create a new element container and add some content
+      // #1 Get data form api and pour to "data"
+      const data = ['option 1', 'option 2', 'option 3']
+      // #2 Convert data to trait option 
+      let traitOptionsData = [];
+      data.forEach(item => {
+        traitOptionsData.push({ id: item, name: item })
+      })
+      // #3 Create HTML selected for trait option 
       const el = document.createElement('div');
       el.innerHTML = `
-        <select class="href-next__type">
-          ${options.map(opt => `<option value="${opt.id}">${opt.name}</option>`).join('')}
+        <select class="options-carousel">
+          ${traitOptionsData.map(opt => `<option value="${opt.id}">${opt.name}</option>`).join('')}
         </select>
-        <div class="href-next__url-inputs" >
-          <input class="href-next__url" placeholder="Insert URL" />
-        </div>
-        <div class="href-next__email-inputs">
-          <input class="href-next__email" placeholder="Insert email" />
-          <input class="href-next__email-subject" placeholder="Insert subject" />
-        </div>
       `;
-  
-      // Let's make our content interactive
-      const inputsUrl = el.querySelector('.href-next__url-inputs');
-      const inputsEmail = el.querySelector('.href-next__email-inputs');
-      const inputType = el.querySelector('.href-next__type');
-      debugger
+      // #4 Add  event => when selected change =
+      const inputType = el.querySelector('.options-carousel');
       inputType.addEventListener('change', ev => {
-        debugger
-        switch (ev.target.value) {
-          case 'url':
-            inputsUrl.style.display = '';
-            inputsEmail.style.display = 'none';
-            break;
-          case 'email':
-            inputsUrl.style.display = 'none';
-            inputsEmail.style.display = '';
-            break;
-        }
+      
       });
-  
       return el;
     },
-    // Get 
+    // THIS FUNCTION WORK WHEN USER CLICK TO TRAIT SETTING or NEXT OF onEvent function  
+    onUpdate({ elInput, component }) {
+      //#1 Get attribute data for update something
+      const dataAttributeValues = component.getAttributes().data || '';
+
+      //#2 Update something here 
+      const inputType = elInput.querySelector('.options-carousel');
+
+      inputType.dispatchEvent(new CustomEvent('change'));
+    },
+    // IN MY OPINION THIS FUNCTION WORK WHEN USER CHANGE OPTION - IF U KNOW IT WORK PLS CMT HERE
     onEvent({ elInput, component, event }) {
-        const attributes = this.model.attributes;
-        const rootElementTrait = elInput;
-        const propertiesOfFrontComponet = component;
-        console.log(rootElementTrait)
-        console.log(propertiesOfFrontComponet)
+      const attributes = this.model.attributes;
+      const rootElementTrait = elInput;
+      const propertiesOfFrontComponet = component;
 
+      //#1 when option change we will get new option => change HTML following option 
+      const inputType = elInput.querySelector('.options-carousel');
+      let data = inputType.value;
 
-        const inputType = elInput.querySelector('.href-next__type');
-        let href = '';
-    
-        switch (inputType.value) {
-          case 'url':
-            const valUrl = elInput.querySelector('.href-next__url').value;
-            href = valUrl;
-            break;
-          case 'email':
-            const valEmail = elInput.querySelector('.href-next__email').value;
-            const valSubj = elInput.querySelector('.href-next__email-subject').value;
-            href = `mailto:${valEmail}${valSubj ? `?subject=${valSubj}` : ''}`;
-            break;
-        }
-        attributes.value = {elInput}
-        component.addAttributes({ href })
-        component.attributes.carouselproperty= href
-      },
-
-      onUpdate({ elInput, component }) {
-        const href = component.getAttributes().href || '';
-        console.log("UPDATE")
-        console.log(elInput)
-        console.log(component)
-        const inputType = elInput.querySelector('.href-next__type');
-        let type = 'url';
-    
-        if (href.indexOf('mailto:') === 0) {
-          const inputEmail = elInput.querySelector('.href-next__email');
-          const inputSubject = elInput.querySelector('.href-next__email-subject');
-          const mailTo = href.replace('mailto:', '').split('?');
-          const email = mailTo[0];
-          const params = (mailTo[1] || '').split('&').reduce((acc, item) => {
-            const items = item.split('=');
-            acc[items[0]] = items[1];
-            return acc;
-          }, {});
-          type = 'email';
-    
-          inputEmail.value = email || '';
-          inputSubject.value = params.subject || '';
-        } else {
-          elInput.querySelector('.href-next__url').value = href;
-        }
-    
-        inputType.value = type;
-        inputType.dispatchEvent(new CustomEvent('change'));
-      },
+      //#2 This function will set attribute data {nameAttribute:Value} => IMPORTAINT FOR COMPONENT LISTEN CHANGE ATTRIBUTE
+      if(component.getAttributes().data != data){
+        component.setAttributes({ data })
+      } 
+    },
   });
-
-
-
+  //THIS IS SETTING COMPONENT
   domc.addType('carousel', {
     model: {
       defaults: {
+        tagName: 'carousel',
         droppable: false,
         traits: [
-        {
-            type: "href-next",
-            name: "carouselproperty",
-            changeProp:true,
-            placeholder: 'Insert text'
-        },
-        {
-            name: 'placeholder',
-            changeProp:true,
-            placeholder: 'Insert text'
-        }
+          {
+            name: 'data',
+            type: "carousel",
+          }
         ],
-        placeholder: 'Initial placeholder',
-        carouselproperty: 'Heeee'
+        // This is default attributes 
+        attributes: {
+          data: 'option 1'
+        }
       },
-      
+      // This function run when component created - we setup listen to change atri
       init() {
-        this.on('change:placeholder', this.handleTypeChange);
-        this.on('change:carouselproperty', this.handleTypeChange);
-        this.on('change:url', this.handleTypeChange);
-        this.on('change:email', this.handleTypeChange);
-        this.listenTo(this, 'change:content', this.handleTypeChange)
+        this.on('change:attributes:data', this.handleTypeChangeData);
+        this.on('change:attributes:placeholder', this.handleTypeChangePlaceHold);
       },
-  
-      handleTypeChange() {
-        console.log('Input type changed to: ');
-        console.log(this.attributes)
+
+      handleTypeChangeData() {
+        console.log("Data")
+        const atributeData = this.attributes.attributes;
+        // IMPORTAINT - this.view.el is root node => form 1 atribute change we can change front end by this
+        this.view.el.innerHTML = atributeData.data
       },
-  },
-  view:{ 
-      init: function() {
+      handleTypeChangePlaceHold() {
+        console.log("placeHold")
+        const atributeData = this.attributes.attributes;
+        // IMPORTAINT - this.view.el is root node => form 1 atribute change we can change front end by this
+        this.view.el.innerHTML = atributeData.data
+      },
+    },
+    view: {
+      init() {
         const attributes = this.model.attributes;
         const rootElement = this.el
-        console.log(rootElement)
-        // Parent div of block
       },
       // Event on layout
       events: {
         click: 'handleClick',
-        dblclick: function(){
+        dblclick: function () {
           alert('Hi!');
         }
       },
-
-      handleClick: function(e) {
+      handleClick: function (e) {
         const attributes = this.model.attributes;
         const rootElement = this.el
-        console.log(attributes);
-        console.log(rootElement)
       },
       render: function () {
         // Extend the original render method
         defaultType.view.prototype.render.apply(this, arguments);
         return this;
-      },  
-  },
+      },
+    },
 
-});
+  });
 
-//LONG-TP 2022-02-22 TEST TRAITS - ADD END 
+  //LONG-TP 2022-02-22 TEST TRAITS - ADD END 
+  //THIS IS SETTING BLOCK
   bm.add("carousel", {
+    // THIS IS HTML DISPLAY ON THE LEFT (BLOCK BAR)
     label: `
     <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg">
     <path d="M14 38h20V8H14v30zM4 34h8V12H4v22zm32-22v22h8V12h-8z"/><path d="M0 0h48v48H0z" fill="none"/>
     </svg>
     <div>${c.carouselBlkLabel}</div> `,
+    // THIS PROPERTY IS CATEGORY GROUP OF BLOCK
     category: c.carousel_category,
     draggable: "[data-gjs-type=wrapper]",
-
     content: [
       {
         name: "Carousel",
         tagName: "carousel",
         //LONG-TP 2022-02-22 TEST TRAITS - ADD START 
-        'type': "carousel",
+        type: "carousel",
         //LONG-TP 2022-02-22 TEST TRAITS - ADD END 
         content: `
-        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel" style="padding:0px">
-          <div class="carousel-      <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img data-js= "carousel" src="https://dummyimage.com/1360x540/55595c/fff" class="d-block w-100" alt="...">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img src="https://dummyimage.com/1360x540/55595c/fff" class="d-block w-100" alt="...">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Second slideindicators">
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-     label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img src="https://dummyimage.com/1360x540/55595c/fff" class="d-block w-100" alt="...">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-              </div>
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://dummyimage.com/1360x540/55595c/fff " class="d-block w-100" alt="https://dummyimage.com/1360x540/55595c/fff ">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-            <div class="carousel-control-prev-icon" aria-hidden="true"></div>
-            <div class="visually-hidden">Previous</div>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-            <div class="carousel-control-next-icon" aria-hidden="true"></div>
-            <div class="visually-hidden">Next</div>
-          </button>
+          <div class="carousel-item">
+            <img src="https://dummyimage.com/1360x540/55595c/fff " class="d-block w-100" alt="https://dummyimage.com/1360x540/55595c/fff ">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://dummyimage.com/1360x540/55595c/fff " class="d-block w-100" alt="https://dummyimage.com/1360x540/55595c/fff ">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
         `,
       },
     ],
   });
 }
+//#endregion

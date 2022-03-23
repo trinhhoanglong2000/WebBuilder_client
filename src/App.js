2369cc9bf0b805cf4e69fb2fab14c729eccd4777
18127168 <tranbui.tainhan1002@gmail.com>
2022-03-23 22:08:30 +0700

    Refactor client

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import Canvas from "./GrapeJs/Canvas";
 import React from "react";
-import Login from "./component/Login";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Register from "./component/Register";
-import HomePage from "./component/HomePage";
 const App = () => {
   return(
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/login' element={<Login/>}/>
           <Route path='/editor/:idStore' element={<Canvas type={2}/>}/>
-          <Route path='/register' element={<Register/>}/>
         </Routes>
       </BrowserRouter>
     </div>

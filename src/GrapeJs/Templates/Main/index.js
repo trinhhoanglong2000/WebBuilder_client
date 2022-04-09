1e08c8f43e53f4118f66e90f79106c5981d93dc8
Trần Phi Long <50954310+goku2000vn@users.noreply.github.com>
2022-04-09 15:48:39 +0700

    Merge branch 'master' of https://github.com/trinhhoanglong2000/WebBuilder_client into CustomBlock-LongTran
    # Conflicts:
    #	src/GrapeJs/Templates/template-default/carousel/index.js
    #	src/GrapeJs/Templates/template-default/template-default.plugins.js

---
+++ src/GrapeJs/Templates/Main/index.js
@@ -0,0 +1,40 @@
+export default function loadBlockMain(editor, opt = {}) {
+  //#region productList
+  const domc = editor.DomComponents;
+
+  domc.addType("Main", {
+    model: {
+      defaults: {
+        highlightable :false,
+        copyable :false,
+        selectable :false,
+        hoverable :false,
+        removable:false,
+        droppable: true,
+      },
+      init() {
+      },
+
+      updated(property, value, prevValue) {
+      },
+      initData() {},
+
+      // This function run when component created - we setup listen to change atri
+    },
+  });
+  editor.BlockManager.add("Main", {
+    label: "Main",
+    category: "Main",
+    content: {
+      name: "Main",
+      
+      type: "Main",
+
+      attributes: {
+        class: "main-content",
+        style: "min-height:100px;width:100%",
+      },
+    },
+  });
+  //#endregion
+}

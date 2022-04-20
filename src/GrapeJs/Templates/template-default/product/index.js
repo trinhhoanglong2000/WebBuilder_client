af8d7d1851606c590d4b10bb97649011e1bf4667
Trịnh Hoàng Long <trinhhoanglong200@gmail.com>
2022-04-21 00:33:26 +0700

    add search bar for product collection

--- src/GrapeJs/Templates/template-default/product/index.js
+++ src/GrapeJs/Templates/template-default/product/index.js
@@ -1,26 +1,32 @@
 import $ from "jquery";
 import { v4 as uuidv4 } from "uuid";
+import AbortController from "abort-controller";
+
 export default function loadBlockProducts(editor, opt = {}) {
+  let controller;
   const c = opt;
   let bm = editor.BlockManager;
-  var GetRequest = async (url)=>{
+  var GetRequest = async (url) => {
+    controller = new AbortController();
     const response = await fetch(url, {
-      method: 'GET', 
+      method: "GET",
 
-    
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      redirect: 'follow', 
+      signal: controller.signal,
+      redirect: "follow",
     });
-    return response.json()
-  }
+    return response.json();
+  };
   //#region productList
   const domc = editor.DomComponents;
   editor.TraitManager.addType("product-collection", {
     // Expects as return a simple HTML string or an HTML element
     createInput({ trait }) {
       const el = document.createElement("div");
+      let initValue = trait.target.attributes.attributes['data-ez-mall-collection'] || "";
+
       el.innerHTML = `
         <div class="Modal-popup dnone" style="">
 
@@ -33,7 +39,7 @@ export default function loadBlockProducts(editor, opt = {}) {
             </button>
           </div>
           <div class="input-group" style="overflow-y: scroll;">
-            <div class="form-outline d-flex w-100 border rounded  mx-1 mt-1" >
+            <div class="form-outline d-flex w-100 border rounded  ml-1 mr-2 mt-1" >
                
                 <input type="search" id="form1" class="form-control pr-0" style="border:none" placeholder = "Search"/>
                 <button type="button " class="btn">
@@ -57,9 +63,9 @@ export default function loadBlockProducts(editor, opt = {}) {
             <div style="font-size: 24px;margin-right: 20px;">
               <i class="fa fa-tags" aria-hidden="true"></i>
             </div>
-            <div style = "overflow: hidden;text-overflow: ellipsis;display: -webkit-box;-webkit-line-clamp: 2; /* number of lines to show */ line-clamp: 2;-webkit-box-orient: vertical;
+            <div class="name-collection" style = "overflow: hidden;text-overflow: ellipsis;display: -webkit-box;-webkit-line-clamp: 2; /* number of lines to show */ line-clamp: 2;-webkit-box-orient: vertical;
           " >
-              SUMMER COLLECTION
+              
             </div>
             </div>
             <div class="type-collection" style="color:rgb(109, 113, 117)">
@@ -83,10 +89,11 @@ export default function loadBlockProducts(editor, opt = {}) {
 
       `;
       el.style = "position:relative";
-      $(el).find(".Modal-popup .close-btn").on('click',function(){
-        $(el).find(".Modal-popup").toggle(200);
-
-      })
+      $(el)
+        .find(".Modal-popup .close-btn")
+        .on("click", function () {
+          $(el).find(".Modal-popup").toggle(200);
+        });
       $(el)
         .find(".dropdown-item")
         .each(function (i) {
@@ -102,36 +109,108 @@ export default function loadBlockProducts(editor, opt = {}) {
           }
           $(this).on("click", func);
         });
-
-        GetRequest(`${process.env.REACT_APP_API_URL}stores/621b5a807ea079a0f7351fb8/collections/product?name=`).then(data => {
-          data.data.forEach(element => {
-            $(el).find('.Modal-popup ul').append(`<li>
+      const GetItem = (name ="",flag=false) => {
+        GetRequest(
+          `${process.env.REACT_APP_API_URL}stores/621b5a807ea079a0f7351fb8/collections/product?name=${name}`
+        ).then((data) => {
+          let domdata = "";
+          initValue = trait.target.attributes.attributes['data-ez-mall-collection'] || "";
+          data.data.forEach((element) => {
+            domdata += `<li data-value = "${element.id}" name="${element.name}" >
             <div style="width: 100%;display: flex;align-items: center;" class="btn border-bottom py-3">
             
               <div class="Picture" >
-                <img style= "width: 32px;height: 32px;" src="${element.thumbnail ? element.thumbnai: 'https://img.icons8.com/fluency-systems-regular/48/000000/image.png'}"/>
+                <img style= "width: 32px;height: 32px;" src="${
+                  element.thumbnail
+                    ? element.thumbnai
+                    : "https://img.icons8.com/fluency-systems-regular/48/000000/image.png"
+                }"/>
 
 
               </div>
-              <div style ="font-size:12px;overflow: hidden;text-overflow: ellipsis;display: -webkit-box;-webkit-line-clamp: 2; /* number of lines to show */ line-clamp: 2;-webkit-box-orient: vertical;
+              <div style ="text-align:left;flex-grow:1;font-size:12px;overflow: hidden;text-overflow: ellipsis;display: -webkit-box;-webkit-line-clamp: 2; /* number of lines to show */ line-clamp: 2;-webkit-box-orient: vertical;
               " >
               ${element.name}
 
               </div>
+              <div class="check-item mr-2">
+              <i class="far fa-check-circle"></i>
+              </div>
             </div>  
             
-            </li>`)
+            </li>`;
           });
-          
-          
+          //  Item section
+          if (!flag)
+            $(el).find(".Modal-popup ul").append(domdata);
+          else
+            $(el).find(".Modal-popup ul").empty().append(domdata);
+
+
+          $(el).find(".Modal-popup ul li").find(".check-item").fadeOut(0);
+          //init
+          $(el).find(`.Modal-popup ul li[data-value="${initValue}"]`).addClass('active')
+          $(el).find(`.Modal-popup ul li[data-value="${initValue}"] .check-item`).fadeIn(0)
+
+          let name = $(el).find(`.Modal-popup ul li[data-value="${initValue}"]`).attr('name')
+          $(el).find(`.card .name-collection`).text(name)
+
+          $(el)
+            .find(".Modal-popup ul li")
+            .hover(
+              function () {
+                if ($(this).hasClass("active")) return;
+                $(this).find(".check-item").fadeIn(100);
+              },
+              function () {
+                if ($(this).hasClass("active")) return;
+                $(this).find(".check-item").fadeOut(100);
+              }
+            );
+          $(el)
+            .find(".Modal-popup ul li")
+            .on("click", function (ev) {
+              $(".Modal-popup ul li").removeClass("active");
+              $(this).addClass("active");
+              $(".Modal-popup ul li").find(".check-item").fadeOut(0);
+              $(".Modal-popup ul li.active").find(".check-item").fadeIn(0);
+
+              let name = $(this).attr('name')
+              $(el).find(`.card .name-collection`).text(name)
+            });
+
+          // 
+          $(el)
+          .find(".Modal-popup ul li")
+          .on("click", (ev) =>{
+            this.onChange(ev)
+          });
+
+        }).catch(function(e) {
+        });;
+      };
+      GetItem()
+      //  INPUT
+      $(el)
+        .find(".Modal-popup input")
+        .on("input", function () {
+          controller.abort();
+
+          GetItem($(this)[0].value,true)
         });
-        // $(el)
-        // .find("input")
-        // .on("input", (ev) => this.onChange(ev));
+
+      // $(el)
+      // .find("input")
+      // .on("input", (ev) => this.onChange(ev));
       return el;
     },
     onEvent({ elInput, component, event }) {
-      console.log("ONEVENT")
+      if (event.type =='change') return
+      const data = $(elInput).find('.Modal-popup ul li.active').data('value')
+      console.log(data)
+      console.log(component)
+      component.setAttributes({...component.getAttributes(),'data-ez-mall-collection':data});
+
       //#1 when option change we will get new option => change HTML following option
     },
   });

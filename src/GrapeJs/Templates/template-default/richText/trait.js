1de05f5755ef48505e713aa7fd6ed92d071f8b4a
Trịnh Hoàng Long <trinhhoanglong200@gmail.com>
2022-06-02 01:03:11 +0700

    add save link trait

--- src/GrapeJs/Templates/template-default/richText/trait.js
+++ src/GrapeJs/Templates/template-default/richText/trait.js
@@ -2,23 +2,9 @@ import $ from "jquery";
 import Quill from "quill";
 import AbortController from "abort-controller";
 import { readCookie } from "../../../../helper/cookie";
+
 export default function loadTraitRichText(editor, opt = {}) {
-  let controller;
 
-  var GetRequest = async (url) => {
-    controller = new AbortController();
-    const response = await fetch(url, {
-      method: "GET",
-
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${readCookie('token')}`,
-      },
-      signal: controller.signal,
-      redirect: "follow",
-    });
-    return response.json();
-  };
   editor.TraitManager.addType("RichText-Button-Color", {
     // Expects as return a simple HTML string or an HTML element
 
@@ -368,12 +354,40 @@ export default function loadTraitRichText(editor, opt = {}) {
   editor.TraitManager.addType("richtext-Btn-Link", {
     // Expects as return a simple HTML string or an HTML element
     createInput({ trait }) {
-      //.Radio-Group CSS in CAnvas CSS
 
+      const debounce = (fn, delay = 1000) => {
+        let timeout;
+        return (...args) => {
+          clearTimeout(timeout);
+          timeout = setTimeout(() => {
+            fn(...args)
+          }, delay);
+        };
+      };
+      var GetRequest = debounce(async (url) => {
+        fetch(url, {
+          method: "GET",
+
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${readCookie('token')}`,
+          },
+          redirect: "follow",
+        })
+          .then(response => response.json())
+          .then(data => {
+            updateUI(data)
+          })
+          .catch(() => { })
+
+      }, 200)
+      const _this = this;
+      const initValue = trait.target.attributes.traitValue.split(';') || "";
+      let defaultIcons=""
       const el = document.createElement("div");
-      let clicked= false
-      let previousValue = ""
-      const Collection_icon= `<svg style =" width:25px;height:25px;" 
+      let clicked = false
+      let previousValue = initValue[1] || ""
+      const Collection_icon = `<svg style =" width:25px;height:25px;" 
       viewBox="0 0 20 20" class="Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true"><path d="M6.948.001c.394 0 .772.159 1.052.439l1.477 1.68-3.638 4.12a3.568 3.568 0 0 0-.872 2.33v9.43h-2.48a1.48 1.48 0 0 1-1.051-.44 1.507 1.507 0 0 1-.436-1.06v-9.88a1.497 1.497 0 0 1 .377-1l3.48-4 1.04-1.18a1.48 1.48 0 0 1 1.052-.439zm7.092 2.439 4.58 5.13c.247.275.383.631.381 1v9.93c0 .399-.159.78-.441 1.062a1.51 1.51 0 0 1-1.065.439h-9.039a1.509 1.509 0 0 1-1.033-.457 1.497 1.497 0 0 1-.423-1.044v-9.88a1.487 1.487 0 0 1 .382-1l3.524-4.001 1.005-1.18a1.51 1.51 0 0 1 2.128 0zm-1.9 5.807a1.51 1.51 0 0 0 1.901-.186 1.497 1.497 0 0 0-.489-2.447 1.512 1.512 0 0 0-1.641.325 1.498 1.498 0 0 0 .228 2.308z"></path></svg>          
       `
       const Products_icon = `<svg style =" width:25px;height:25px;" 
@@ -385,9 +399,24 @@ export default function loadTraitRichText(editor, opt = {}) {
       const Privacy_icon = `<svg style =" width:25px;height:25px;" 
       viewBox="0 0 20 20" class="Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true"><path d="M7 5h5v2h-5v-2zm5 4h-5v2h5v-2z"></path><path fill-rule="evenodd" d="M16 17a3 3 0 0 1-3 3h-10a3 3 0 0 1-3-3v-1.5a1.5 1.5 0 0 1 1.5-1.5h1.5v-10a3 3 0 0 1 3-3h11a3 3 0 1 1 0 6h-1v10zm-11-13a1 1 0 0 1 1-1h8.17c-.11.313-.17.65-.17 1v13a1 1 0 1 1-2 0v-3h-7v-10zm12-1a1 1 0 0 0-1 1v1h1a1 1 0 1 0 0-2zm-7 14c0 .35.06.687.17 1h-7.17a1 1 0 0 1-1-1v-1h8v1z"></path></svg>
       `
+      if (initValue[0] == "Collections") {
+        defaultIcons =Collection_icon
+      }
+      else if (initValue[0] == "Products") {
+        defaultIcons =Products_icon
+
+      }
+      else if (initValue[0] == "Pages") {
+        defaultIcons =Pages_icon
+
+      }
+      else if (initValue[0] == "Privacy") {
+        defaultIcons =Privacy_icon
+
+      }
       const defaultMenu_Collection = `
       
-        <li data-value ="Collection" class="btn" style="text-align:start;padding-top:5px;padding-bottom:5px">
+        <li data-value ="Collections" class="btn" style="text-align:start;padding-top:5px;padding-bottom:5px">
           ${Collection_icon}
           <span style="white-space: nowrap; overflow: hidden;text-overflow: ellipsis;margin-left:10px">
             Collections
@@ -429,15 +458,18 @@ export default function loadTraitRichText(editor, opt = {}) {
       el.innerHTML = `
       <div id= "Link-combo" class="combo gjs-field gjs-field-text">
         <div style="width:100%;display:flex;align-items:center;position:relative;">
-          <div id="icons" style = "position: absolute;z-index: 2;margin-left: 10px;${previousValue===''?'display:none':''}">
-          <svg  style =" width:25px;height:25px;" 
-          viewBox="0 0 20 20" class="Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true"><path d="M12.44.44a1.5 1.5 0 0 0-1.062-.44h-6.878a1.5 1.5 0 0 0-1.5 1.5v17a1.5 1.5 0 0 0 1.5 1.5h11a1.5 1.5 0 0 0 1.5-1.5v-12.879a1.5 1.5 0 0 0-.44-1.06l-4.12-4.122z"></path></svg>
-          
+          <div id="icons" style = "position: absolute;z-index: 2;margin-left: 10px;${previousValue === '' ? 'display:none' : ''}">
+          ${defaultIcons}
           </div>
-          <input style = "${previousValue===''?'':'padding-left: 39px;'}" type="text" id="state" value="" size="10" placeholder="Paste a link or search" autocomplete="off">
+          <input style = "${previousValue === '' ? '' : 'padding-left: 39px;padding-right:25px'}" type="text" id="state" value="${previousValue}" size="10" placeholder="Paste a link or search" autocomplete="off">
+          <div id="delete_icon" class="" style= " z-index: 2; position: absolute; right: 0;margin-right: 5px;${previousValue === '' ? 'display:none' : ''}" >
+          <svg  style =" width:20px;height:20px;" 
+          viewBox="0 0 20 20" class="Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true"><path fill-rule="evenodd" d="M10 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm-2.293 4.293a1 1 0 0 0-1.414 1.414l2.293 2.293-2.293 2.293a1 1 0 1 0 1.414 1.414l2.293-2.293 2.293 2.293a1 1 0 1 0 1.414-1.414l-2.293-2.293 2.293-2.293a1 1 0 0 0-1.414-1.414l-2.293 2.293-2.293-2.293z"></path></svg>
+          </div>
         </div>
         <ul class="combobox-hidden">
          ${Backbutton}
+
          <div id="Link-menu">
          
          </div>
@@ -453,10 +485,10 @@ export default function loadTraitRichText(editor, opt = {}) {
         GetItem()
       })
 
-      const GetItem = (name = "", flag = false) => {
+      const GetItem = async (name = "", flag = false) => {
         name = name.trim()
         if (State == "Main-Menu") {
-          const arr = ["Collection", "Products", "Pages", "Privacy"]
+          const arr = ["Collections", "Products", "Pages", "Privacy"]
           let domdata = "";
           const regex = new RegExp(`.*${name.toUpperCase()}.*`, 'g');
           arr.forEach((ele, index) => {
@@ -489,17 +521,23 @@ export default function loadTraitRichText(editor, opt = {}) {
 
           $(el).find('#Link-menu li').on('click', function () {
             clicked = true
-            console.log(clicked)
             State = $(this).data('value')
             // $(el).find('input').val("")
             $(el).find('input').focus()
-            
+
             GetItem()
           })
         }
         else {
+          if ($(el).find('#loadingDiv').length === 0) {
+            $(el).find("#Link-menu").empty().append(`<div id="loadingDiv">
+            <div ></div>
+          </div>`);
+          }
+          $(el).find('#Back-btn').removeClass('d-none')
+
           let url = ""
-          if (State == "Collection") {
+          if (State == "Collections") {
             url = `stores/${opt.storeId}/collections/product?name=${name.trim()}`
           }
           else if (State == "Products") {
@@ -513,121 +551,156 @@ export default function loadTraitRichText(editor, opt = {}) {
           }
           GetRequest(
             `${process.env.REACT_APP_API_URL}${url}`
-          ).then((data) => {
-            let _data = []
-            if (State == "Collection") {
-              _data = data.data.map(ele => {
-                return {
-                  name: ele.name,
-                  id: ele.id,
-                  thumbnail: ele.thumbnail
-                }
-              })
-            }
-            let domdata = "";
-            _data.forEach((element) => {
-              const img = element.thumbnail ? `
-              <img style= "width:25px;height:25px;" src="${element.thumbnail}">
-              `: `
-              <svg style =" width:25px;height:25px;" 
-                viewBox="0 0 20 20" class="Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true"><path fill-rule="evenodd" d="M1 2.5a1.5 1.5 0 0 1 1.5-1.5h15a1.5 1.5 0 0 1 1.5 1.5v12a1.5 1.5 0 0 1-1.5 1.5h-15a1.5 1.5 0 0 1-1.5-1.5v-12zm8 2.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm6.57 9h-11.143c-.351 0-.548-.368-.343-.632l3.046-3.24a.448.448 0 0 1 .617-.009l1.396 1.481 2.623-3.825a.446.446 0 0 1 .72.016l3.462 5.609c.154.272-.052.6-.377.6z"></path><path d="M6 20a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm5-1a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"></path></svg>
-                
-              `
-              domdata += `
-                <li data-value ="Collection/${element.id}" class="btn" style="text-align:start;padding-top:5px;padding-bottom:5px">
-                ${img}
-                <span style="white-space: nowrap; overflow: hidden;text-overflow: ellipsis;margin-left:10px">
-                    ${element.name}
-                </span>
-                </li>    
-              `;
-            });
-            if (domdata === "") {
-              domdata += `
-              <li data-value ="Products" class="btn" style="text-align:start;padding-top:5px;padding-bottom:5px;pointer-events:none">
-              <span style="white-space: nowrap; overflow: hidden;text-overflow: ellipsis;margin-left:10px">
-                No results
-              </span>
-            </li>
-            `
-            }
-            $(el).find('#result').text(`${_data.length} results`)
-            $(el).find("#Link-menu").empty().append(domdata);
-            $(el).find('#Back-btn').removeClass('d-none')
-            $(el).find('#Link-menu li').on('click', function () {
-              const text = $(this).find('span').text().trim()
-              $(el).find('input').val(text)
-              
-              // $(el).find('input').focus()
-              $(el).find('ul').addClass('combobox-hidden')
-              $(el).find('#Back-btn').addClass('d-none')
-              previousValue = text
-              if (previousValue!==""){
-                $(el).find('input').css('padding-left','39px');
-                $(el).find('#icons').css("display","block")
-                if (State == "Collection") {
-                  $(el).find('#icons').empty().append(Collection_icon)
-                }
-                else if (State == "Products") {
-      
-                }
-                else if (State == "Pages") {
-      
-                }
-                else if (State == "Privacy") {
-      
-                }
-              }
-              else{
-                $(el).find('input').css('padding-left','');
-                $(el).find('#icons').css("display","none")
-              }
-              State = "Main-Menu"
+          )
 
-              clicked = false
-            })
-
-          }).catch(function (e) {
-          });
         }
 
       };
+
+      const updateUI = (data) => {
+        let _data = []
+        if (State == "Collections") {
+          _data = data.data.map(ele => {
+            return {
+              name: ele.name,
+              id: ele.id,
+              thumbnail: ele.thumbnail
+            }
+          })
+        }
+        let domdata = "";
+        _data.forEach((element) => {
+          const img = element.thumbnail ? `
+        <img style= "width:25px;height:25px;" src="${element.thumbnail}">
+        `: `
+        <svg style =" width:25px;height:25px;" 
+          viewBox="0 0 20 20" class="Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true"><path fill-rule="evenodd" d="M1 2.5a1.5 1.5 0 0 1 1.5-1.5h15a1.5 1.5 0 0 1 1.5 1.5v12a1.5 1.5 0 0 1-1.5 1.5h-15a1.5 1.5 0 0 1-1.5-1.5v-12zm8 2.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm6.57 9h-11.143c-.351 0-.548-.368-.343-.632l3.046-3.24a.448.448 0 0 1 .617-.009l1.396 1.481 2.623-3.825a.446.446 0 0 1 .72.016l3.462 5.609c.154.272-.052.6-.377.6z"></path><path d="M6 20a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm5-1a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"></path></svg>
+          
+        `
+          domdata += `
+          <li data-value ="${State.toLowerCase()}/${element.id}" class="btn" style="text-align:start;padding-top:5px;padding-bottom:5px">
+          ${img}
+          <span style="white-space: nowrap; overflow: hidden;text-overflow: ellipsis;margin-left:10px">
+              ${element.name}
+          </span>
+          </li>    
+        `;
+        });
+        if (domdata === "") {
+          domdata += `
+        <li data-value ="Products" class="btn" style="text-align:start;padding-top:5px;padding-bottom:5px;pointer-events:none">
+        <span style="white-space: nowrap; overflow: hidden;text-overflow: ellipsis;margin-left:10px">
+          No results
+        </span>
+      </li>
+      `
+        }
+        $(el).find('#result').text(`${_data.length} results`)
+        $(el).find("#Link-menu").empty().append(domdata);
+
+        $(el).find('#Link-menu li').on('click',  function () {
+          const text = $(this).find('span').text().trim()
+          $(el).find('input').val(text)
+
+          // $(el).find('input').focus()
+          $(el).find('ul').addClass('combobox-hidden')
+          $(el).find('#Back-btn').addClass('d-none')
+          previousValue = text
+          if (previousValue !== "") {
+            $(el).find('input').css('padding-left', '39px');
+            $(el).find('#icons').css("display", "block")
+            
+            $(el).find('input').css('padding-right', '25px');
+            $(el).find('#delete_icon').css("display", "block")
+
+            if (State == "Collections") {
+              $(el).find('#icons').empty().append(Collection_icon)
+            }
+            else if (State == "Products") {
+              $(el).find('#icons').empty().append(Products_icon)
+
+            }
+            else if (State == "Pages") {
+              $(el).find('#icons').empty().append(Pages_icon)
+
+            }
+            else if (State == "Privacy") {
+              $(el).find('#icons').empty().append(Privacy_icon)
+
+            }
+          }
+          else {
+            $(el).find('input').css('padding-left', '');
+            $(el).find('#icons').css("display", "none")
+
+            $(el).find('input').css('padding-left', '');
+            $(el).find('#delete_icon').css("display", "none")
+          }
+          _this.onChange({valueHref:$(this).data('value'),traitValue:`${State};${previousValue}`})
+
+          State = "Main-Menu"
+          clicked = false
+        })
+
+      }
+      // ==============================
+      $(el).find('#delete_icon').on('click',function(){
+        const text = ""
+        $(el).find('input').val(text)
+        $(el).find('ul').addClass('combobox-hidden')
+        $(el).find('#Back-btn').addClass('d-none')
+        previousValue = text
+        $(el).find('input').css('padding-left', '');
+        $(el).find('#icons').css("display", "none")
+
+        $(el).find('input').css('padding-left', '');
+        $(el).find('#delete_icon').css("display", "none")
+        State = "Main-Menu"
+        clicked = false
+        _this.onChange({valueHref:"",traitValue:""})
+      })
       GetItem()
       $(el).find('input').focusin(function () {
         $(el).find('ul').removeClass('combobox-hidden')
-        
+
       })
       $(el).find('input').focusout(function (e) {
         $(el).find('ul').addClass('combobox-hidden')
-        setTimeout(()=>{
-          if (!clicked){
+        setTimeout(() => {
+          if (!clicked) {
             GetItem()
-          }else{
-            clicked=false;
+          } else {
+            clicked = false;
           }
-        },200)
+        }, 200)
 
         $(el).find('input').val(previousValue)
-        // if (previousValue!==""){
-        //   $(el).find('input').css('padding-left','39px');
-        //   $(el).find('#icons').css("display","block")
-        // }
-        // else{
-        //   $(el).find('input').css('padding-left','');
-        //   $(el).find('#icons').css("display","none")
-        // }
-        //GetItem()
-
+       
       })
+
       $(el).find('input').on("input", function (ev) {
-        if (controller)
-          controller.abort();
+        if ($(this)[0].value){
+          $(el).find('input').css('padding-right', '25px');
+          $(el).find('#delete_icon').css("display", "block")
+        }
+        else{
+          $(el).find('input').css('padding-right', '');
+          $(el).find('#delete_icon').css("display", "none")
+        }
         GetItem($(this)[0].value, true)
       })
       return el;
     },
     onEvent({ elInput, component, event }) {
-      console.log("HEHE")
+      if (event.type){
+        return
+      }
+      const value = event.valueHref ? event.valueHref : '#'
+      component.setAttributes({ ...component.getAttributes(), 'href': value })
+      component.set('traitValue',event.traitValue)
+      console.log(elInput)
+      console.log(component)
+      console.log(event)
     },
   });
 }

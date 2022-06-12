d639392f67053d7594c6d065c2496526ddbe2048
Trịnh Hoàng Long <trinhhoanglong200@gmail.com>
2022-06-13 00:51:59 +0700

    change page navigation

--- src/GrapeJs/commands/index.js
+++ src/GrapeJs/commands/index.js
@@ -14,61 +14,70 @@ export default function LoadCommands(editor, config, opt) {
   const cm = editor.Commands;
   const txtConfirm = config.textCleanCanvas;
   //======================|| =============
-  const beforeSaveData = async() => {
+  const beforeSaveData = async () => {
     const asset = editor.AssetManager.getAll().models;
     const imagesUpload = [];
     const target = [];
-  
+
     asset.forEach((element, index) => {
       if (!validURL(element.get("src"))) {
         imagesUpload.push(element.get("src"));
         target.push(index);
-      } 
+      }
     })
 
     if (imagesUpload.length > 0) {
       const response = await callAPIWithPostMethod("files/upload-image-to-s3", { data: imagesUpload }, false);
-      
-      response && response.data && response.data.forEach( (element, index) => {
+
+      response && response.data && response.data.forEach((element, index) => {
         editor.AssetManager.getAll().models[target[index]].set("src", element);
-        opt.renderImage({id: asset[target[index]].cid, image: element})
-      }) 
+        opt.renderImage({ id: asset[target[index]].cid, image: element })
+      })
     }
   }
-    //======================|| =============
+  //======================|| =============
 
   cm.add(cmdDeviceDesktop, (e) => e.setDevice("Desktop"));
   cm.add(cmdDeviceTablet, (e) => e.setDevice("Tablet"));
   cm.add(cmdDeviceMobile, (e) => {
-  e.setDevice("Mobile portrait")
-  // const component = e.getSelected();
-  // const coll = component.collection;
-  // const at = coll.indexOf(component);
-  // coll.remove(component);
+    e.setDevice("Mobile portrait")
+    // Delete select
+    // const component = e.getSelected();
+    // const coll = component.collection;
+    // const at = coll.indexOf(component);
+    // coll.remove(component);
 
-});
+    // Draggable
+    // const footer = editor.getComponents().where({ name: 'Footer' })[0];
+    // const header = editor.getComponents().where({ name: 'Header' })[0];
+    // const main   = editor.getComponents().where({ name: "Main" })[0]
+    // footer.set({ draggable: false,removable:false })
+    // header.set({ draggable: false,removable:false })
+    // main.set(  { draggable: false,removable:false })
+
+  });
   cm.add(cmdClear, (e) => window.confirm(txtConfirm) && e.runCommand("core:canvas-clear"));
-  cm.add(cmdTogglePreview, async(e) => {
+  cm.add(cmdTogglePreview, async (e) => {
     const isActive = e.Commands.isActive('preview')
-    if (!isActive){
+    if (!isActive) {
       e.Commands.run('preview')
     }
-    else{
+    else {
       e.Commands.stop('preview')
     }
-  },)
-  cm.add(save, async(e) => {
+  })
+  cm.add(save, async (e) => {
     await beforeSaveData();
 
     e.store();
-  },)
+  })
   cm.add(openBlock, (e) => {
     var element = document.getElementsByClassName("gjs-pn-block-container");
     var elementCanvas = document.getElementsByClassName("gjs-cv-canvas");
     //====Add transition
     element[0].classList.add("trans")
     elementCanvas[0].classList.add("trans")
-    
+
     for (var i = 0; i < element.length; i++) {
       element[i].classList.toggle("active-block");
     }
@@ -84,7 +93,7 @@ export default function LoadCommands(editor, config, opt) {
         element[0].classList.remove("trans")
         elementCanvas[0].classList.remove("trans")
 
-        
+
         editor.refresh();
       });
     }
--- src/GrapeJs/pages/NavigationPanel/index.js
+++ src/GrapeJs/pages/NavigationPanel/index.js
@@ -1,25 +1,74 @@
 import "./index.css";
+import React, { useState } from "react";
+import { DropDown_icon, Home_icon } from "../../../asset/icon/svg"
+const NavigationPanel = ({ listPagesId, setLoading, setSearchParams, pageId }) => {
+    const [expanded, setExpanded] = useState(false);
+    const [name, setName] = useState(listPagesId.filter((ele) => ele.id === pageId)[0]?.name)
 
-const NavigationPanel = ({listPagesId, setLoading, setSearchParams,  pageId}) => {
+    function expand() {
+        setExpanded(true);
+    }
+
+    function close() {
+        setExpanded(false);
+    }
 
     //=======================FUNCTION=========================
     const handleOnchangePage = (e) => {
+
+        close();
+
         setLoading(true);
-        
-        setSearchParams({pageId:  e.target.value})
+        setName(e.target.dataset.name)
+        setSearchParams({ pageId: e.target.dataset.value })
+
     }
 
+    // const renderPagesItem = () => {
+    //     return listPagesId.map((element) => { return <option value={element.id} key={element.id}> {element.name} </option> });
+    // }
     const renderPagesItem = () => {
-        return listPagesId.map((element) => { return <option value={element.id} key={element.id}> {element.name} </option> });
-    }
+        return listPagesId.map((element) => {
+            if (element.id === pageId && !name) setName(element.name)
+            return <li onClick={handleOnchangePage} data-name={element.name} data-value={element.id} key={element.id}>
+                <div style={{
+                    width: '25px',
+                    height: '25px',
+                }} dangerouslySetInnerHTML={{ __html: Home_icon }}>
 
+                </div> 
+                <div>
+                {element.name}
+
+                </div>
+            </li>
+        });
+    }
     return (
         <div className="navigationPanel" id="navigationPanelPages">
-            <label>Pages: 
-                <select name="pages" id="pages" value={pageId} onChange={handleOnchangePage}>
-                    {listPagesId && renderPagesItem()}
-                </select>
-            </label>
+            <div style={{ width: '100%', 'marginTop': '13px' }} tabIndex={0} onMouseDown={expand} onBlur={close}>
+                <div className="navigtionWrapper btn" >
+                    {name && <p>{name}</p>}
+
+                    <div style={{
+                        width: '30px',
+                        height: '30px',
+                    }} className="content" dangerouslySetInnerHTML={{ __html: DropDown_icon }}></div>
+                   
+                </div>
+                {/* <ul className={"dropdown-options-list"}>
+                        {listPagesId && renderPagesItem()}
+                    </ul> */}
+                {expanded ? (
+                    <ul className={"dropdown-options-list"}>
+                        {listPagesId && renderPagesItem()}
+                    </ul>
+                ) : null}
+                {/* <select name="pages" id="pages" value={pageId} onChange={handleOnchangePage}>
+                {listPagesId && renderPagesItem()}
+            </select> */}
+
+            </div>
         </div>
     )
 }

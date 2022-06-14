31e7c7509eaea1580fa86cbe72e4f222a56aacbc
HDS\tlong <trinhhoanglong200@gmail.com>
2022-06-14 18:00:36 +0700

    add skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Canvas from "./GrapeJs/Canvas";
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import SaveLoad from './components/SaveLoad/SaveLoad'
 const App = () => {
   return(
     <div>
       <BrowserRouter>
         <Routes >
           <Route path='/editor/:idStore' element={<Canvas type={2}/>}/>
+          <Route path='/load' element={<SaveLoad open={true} />}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/SaveLoad/SaveLoad.js
+++ src/components/SaveLoad/SaveLoad.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
-const SaveLoad = ({open}) => {
+import Skeleton from '@mui/material/Skeleton';
+import './Saveload.css'
+const SaveLoad = ({ open }) => {
   return (
     <Backdrop
-      sx={{ color: "rgb(25, 118, 210)", zIndex: (theme) => theme.zIndex.drawer + 1,opacity:1,background: '#f6faf6' }}
+      sx={{ color: "rgb(25, 118, 210)", zIndex: (theme) => theme.zIndex.drawer + 1, opacity: 1, background: '#f6faf6' }}
       open={open}
     >
-      <CircularProgress color="inherit"  sx={{width:"25vh!important",height:"25vh!important"}}/>
+      <CircularProgress color="inherit" sx={{ width: "25vh!important", height: "25vh!important"}} />
+      <div className="NavigationPanel-Skeleton">
+        <Skeleton className="block-panel-Skeleton" variant="rectangular" width={30} height={30} animation="wave" />
+        <div className="device-panel-Skeleton" >
+          <Skeleton sx={{ marginRight: '10px' }} variant="rectangular" width={30} height={30} animation="wave" />
+          <Skeleton sx={{ marginRight: '10px' }} variant="rectangular" width={30} height={30} animation="wave" />
+          <Skeleton sx={{ marginRight: '10px' }} variant="rectangular" width={30} height={30} animation="wave" />
+        </div>
+
+      </div>
     </Backdrop>
   );
 };

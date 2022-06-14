d67f11e5070f0bbcf70604bb7733d6e87e396e1f
HDS\tlong <trinhhoanglong200@gmail.com>
2022-06-14 17:26:47 +0700

    fix color for loading

--- src/components/SaveLoad/SaveLoad.js
+++ src/components/SaveLoad/SaveLoad.js
@@ -4,7 +4,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 const SaveLoad = ({open}) => {
   return (
     <Backdrop
-      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1,opacity:1 }}
+      sx={{ color: "rgb(25, 118, 210)", zIndex: (theme) => theme.zIndex.drawer + 1,opacity:1,background: '#f6faf6' }}
       open={open}
     >
       <CircularProgress color="inherit"  sx={{width:"25vh!important",height:"25vh!important"}}/>

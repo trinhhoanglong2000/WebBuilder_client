253c160cb7be8a26f494d3e88e11fdd1bb3ef016
Trịnh Hoàng Long <trinhhoanglong200@gmail.com>
2022-07-22 01:47:24 +0700

    add sh1t

--- src/GrapeJs/Templates/template-default/trackingOrderForm/index.js
+++ src/GrapeJs/Templates/template-default/trackingOrderForm/index.js
@@ -6,45 +6,51 @@ export default function loadTrackingOrderForm(editor, opt = {}) {
     const dc = editor.DomComponents;
   
     const defaultType = dc.getType("default");
-
-    bm.add("tracking-order-form", {
-        label: `${TRACKING_ICON}
-            <div>Tracking Order Form</div>`,
-        category: "Tracking Order",
-        content: {
-            type: "tracking-Order-Form",
-            components: [
-            {
-                type: "defaultCustom",
-                tagName: "div",
-                attributes: { class: "card" },
-                content: `
-                    <h1 class="card-header"> TRACKING ORDER </h1>
-                        <div class="card-body">
-                        <P> Tracking your order by enter the information below.</P>
-                        <p> We need your email address and order code for security </p>
-                        <div class="input-form">
-                            <h5 class="error"> </h5>
-                            <h5 class="notify"> </h5>
-                            <input id="email" class="form-control" placeholder="Email">
-                            <input id="orderCode" class="form-control" placeholder="Order code">
+    if (!opt.isDeloy) {
+        bm.add("tracking-order-form", {
+            label: `${TRACKING_ICON}
+                <div>Tracking Order Form</div>`,
+            category: "Tracking Order",
+            content: {
+                type: "tracking-Order-Form",
+                components: [
+                {
+                    type: "defaultCustom",
+                    tagName: "div",
+                    attributes: { class: "card" },
+                    content: `
+                        <h1 class="card-header"> TRACKING ORDER </h1>
+                            <div class="card-body">
+                            <P> Tracking your order by enter the information below.</P>
+                            <p> We need your email address and order code for security </p>
+                            <div class="input-form">
+                                <h5 class="error"> </h5>
+                                <h5 class="notify"> </h5>
+                                <input id="email" class="form-control" placeholder="Email">
+                                <input id="orderCode" class="form-control" placeholder="Order code">
+                            </div>
+                            <div class="footer-btn">
+                                <button type="button" class="btn btn-submit" id="submitBtn"> Find Order </button>
+                            </div>
                         </div>
-                        <div class="footer-btn">
-                            <button type="button" class="btn btn-submit" id="submitBtn"> Find Order </button>
-                        </div>
-                    </div>
-                `
+                    `
+                }
+                ]
             }
-            ]
-        }
-    })
+        })
+    }
 
     dc.addType("tracking-Order-Form", {
         model: {
             defaults: {
                 name: "Tracking Order Form",
-                draggable: ".main-content",
+                // draggable: ".main-content",
+                draggable: false,
                 droppable: false,
+                copyable: false,
+                selectable: false,
+                hoverable: false,
+                removable: false,
                 copyable: false,
                 attributes: { class: "container", name: "trackingOrderForm", slideIndex: 1 },
                 traits: [
